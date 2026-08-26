b7f50f49e13f1e6e7d7d84df0d127009d0e3155f
Anthony Mutahi <mutahitony28@gmail.com>
2026-08-26 23:31:17 +0300

    Move stay-alive-cms out of public folder to reduce app bundle size

--- public/stay-alive-cms/sanityClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://esm.sh/@sanity/client';
-
-export const client = createClient({
-  projectId: 'y4q1h6a9',
-  dataset: 'production',
-  useCdn: true,
-  apiVersion: '2024-01-01'
-});
